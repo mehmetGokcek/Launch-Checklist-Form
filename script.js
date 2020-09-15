862c53c1f752461126282c2b53025fc2638ffca3
Mehmet Gokcek <gokcek50@gmail.com>
2020-09-14 23:23:15 -0500

    form validation completed

--- script.js
+++ script.js
@@ -11,3 +11,49 @@
 </ol>
 <img src="${}">
 */
+
+
+//isLetter will return true only in case of letter
+function isLetter(name) {
+    let result = false;
+    for (let i = 0; i < name.length; i++) {
+        if (name[i].toUpperCase() != name[i].toLowerCase()) {
+            result = true;
+        } else {
+            result = false;
+        }
+    }
+    return result;
+}
+
+
+window.addEventListener("load", function() {
+    let form = document.querySelector("form");
+    form.addEventListener("submit", function(event) {
+        let pilotName = document.querySelector("input[name=pilotName]");
+        let copilotName = document.querySelector("input[name=copilotName]");
+        let fuelLevel = document.querySelector("input[name=fuelLevel]");
+        let cargoMass = document.querySelector("input[name=cargoMass]");
+
+
+        if (pilotName.value === "" ||
+            copilotName.value === "" ||
+            fuelLevel === "" ||
+            cargoMass === "") {
+            alert("All fields are required!");
+            // stop the form submission
+            event.preventDefault();
+        } else {
+
+            if (!isLetter(pilotName.value) || !isLetter(copilotName.value)) {
+                alert("Error:Please use only letters for name fields!");
+                event.preventDefault();
+
+                //isNaN: Not a Number, returns true if the value is not a number
+            } else if (isNaN(fuelLevel.value) || isNaN(cargoMass.value)) {
+                alert("Error: Please use only numbers for Fuel Level and Cargo Mass fields!");
+                event.preventDefault();
+            }
+        }
+    });
+});
